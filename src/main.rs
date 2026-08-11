a2bf21cae2b7cb5ab7919237607b80d54d79e901
OriPC-wsl <j904forade8979@outlook.jp>
2026-08-11 19:14:48 +0900

    画像をwebpに変換する前にメタデータにあるGPS情報を消去する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use exiftool_rs::ExifTool;
 use image::ImageFormat;
 use std::env;
 use std::error::Error;
@@ -18,6 +19,40 @@ fn convert_image(
 
     println!("画像の読み込みに成功");
     img.save_with_format(output_img, format)?;
+    Ok(())
+}
+
+fn remove_tags(input_img: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut exiftool = ExifTool::new();
+    let tags = match exiftool.extract_info(input_img) {
+        Ok(tags) => tags,
+        Err(error) => {
+            eprintln!("メタデータの取得に失敗しました: {error}");
+            return Ok(());
+        }
+    };
+
+    for tag in &tags {
+        println!("{}: {}", tag.name, tag.print_value);
+        if tag.name.starts_with("GPS") {
+            println!("消去対象: {}", tag.name);
+            exiftool.set_new_value(&tag.name, None);
+        }
+    }
+
+    match exiftool.write_info(input_img, input_img) {
+        Ok(_) => println!("GPS関連のタグを消去しました"),
+        Err(error) => eprintln!("書き込みに失敗しました: {error}"),
+    }
+
+    println!("{}", "-".repeat(30));
+    println!("消去できたかチェック");
+    for tag in &tags {
+        if tag.name.starts_with("GPS") {
+            println!("{}: {}", tag.name, tag.print_value);
+        }
+    }
+
     Ok(())
 }
 
@@ -47,6 +82,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("入力ファイル: {}", input_img);
     println!("{:?}", output_format);
+    println!("{}", "-".repeat(30));
+    remove_tags(input_img)?;
+    println!("{}", "-".repeat(30));
     convert_image(input_img, output_img, output_format)?;
     println!("変換完了");
     Ok(())
