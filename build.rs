0a605bfec8dc5e59c04866e3f59faa6007dff614
OriPC <j904forade8979@outlook.jp>
2026-08-22 17:51:06 +0900

    アイコンファイルをJPEGからICO形式に変更

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
 
-        //res.set_icon("assets/icon.jpg");
+        res.set_icon("assets/icon.ico");
         res.set_language(0x411);
         res.set("FileDescription", "画像変換・メタデータ削除ツール");
         res.set("ProductName", "Image converter tool");
