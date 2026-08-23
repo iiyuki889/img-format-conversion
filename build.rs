a674a384b241666199562c3d782a3c67507dcd44
OriPC <j904forade8979@outlook.jp>
2026-08-24 01:17:02 +0900

    バージョンを0.1.2に更新し、アイコンファイルのパスを修正

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
+    println!("cargo:rerun-if-changed=assets/icon.ico");
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
 
-        //res.set_icon(r"assets\icon.ico");
+        res.set_icon(r"assets\iconv2.ico");
         res.set_language(0x411);
         res.set("FileDescription", "画像変換・メタデータ削除ツール");
         res.set("ProductName", "Image converter tool");
